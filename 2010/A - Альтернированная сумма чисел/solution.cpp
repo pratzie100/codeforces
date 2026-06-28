b6767da646b1be043522cb7d68a4c4d86bbb3b34
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-06-28 17:23:38 +0530

    Add Альтернированная сумма чисел [A] from Codeforces

---
+++ 2010/A - Альтернированная сумма чисел/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define endl "
+"
+//#define mod  1000000000000000007LL;  
+// const ll mod2= 1000000000000000007LL; 
+ 
+const ll MOD = 998244353LL;
+ 
+ 
+ 
+void solve()
+{
+    int n;
+    cin>>n;
+    ll sum=0;
+    for(int i=0;i<n;i++) {
+        int x;
+        cin>>x;
+        if(i&1) sum-=x;
+        else sum+=x;
+    }
+    cout<<sum<<endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+   int t; 
+   cin>>t; 
+   while(t--)
+   { 
+       solve();
+    }
+ 
+ 
+  
+    // solve();
+ 
+   return 0;    
+}
+ 
+ 
