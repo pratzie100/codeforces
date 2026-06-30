f22d682becea28bcdc648455485d3340b6e88c38
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-06-30 16:57:46 +0530

    Add Игра с числами [A] from Codeforces

---
+++ 1899/A - Игра с числами/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define endl "
+"
+//#define mod  1000000000000000007LL;  
+// const ll mod2= 1000000000000000007LL; 
+ 
+void solve()
+{
+    int n;
+    cin>>n;    
+    if(n%3==0) cout<<"Second"<<endl;
+    else cout<<"First"<<endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+   int t; 
+   cin>>t; 
+   while(t--)
+   { 
+       solve();
+    }
+ 
+ 
+  
+    // solve();
+    
+    ///*@CodingBoy100*/
+   return 0;    
+}
+ 
+ 
