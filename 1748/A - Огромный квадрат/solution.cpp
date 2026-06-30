dc70ec11d713361d13b5ed5083267b88cee76e6b
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-06-30 11:08:59 +0530

    Add Огромный квадрат [A] from Codeforces

---
+++ 1748/A - Огромный квадрат/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define endl "
+"
+//#define mod  1000000000000000007LL;  
+// const ll mod2= 1000000000000000007LL; 
+ 
+void solve()
+{
+    int n;
+    cin>>n;
+ 
+    cout<< int(ceil(n/2.0))<<endl;
+ 
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+   int t; 
+   cin>>t; 
+   while(t--)
+   { 
+       solve();
+    }
+ 
+ 
+  
+    // solve();
+    
+    ///*@CodingBoy100*/
+   return 0;    
+}
+ 
+ 
