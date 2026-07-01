68ad64c81e259fa470e1097027b6710b80249f51
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-07-01 19:45:55 +0530

    Add Голы победы [A] from Codeforces

---
+++ 1877/A - Голы победы/solution.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define endl "
+"
+//#define mod  1000000000000000007LL;  
+// const ll mod2= 1000000000000000007LL; 
+ 
+void solve()
+{
+    int n;
+    cin>>n;
+    ll s=0;
+    //sum of efficiencies has to be 0
+    for(int i=0;i<n-1;i++){
+        int x;
+        cin>>x;
+        s+=x;
+    }
+    cout<<-s<<endl;
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+   int t; 
+   cin>>t; 
+   while(t--)
+   { 
+       solve();
+    }
+ 
+ 
+  
+    // solve();
+    
+    ///*@CodingBoy100*/
+   return 0;    
+}
+ 
+ 
