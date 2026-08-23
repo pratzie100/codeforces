b5403735d10028df2bfb0affec3a688c8af11ac7
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-08-23 11:34:21 +0530

    Add Химия [B] from Codeforces

--- 1883/B - Химия/solution.cpp
+++ 1883/B - Химия/solution.cpp
@@ -1,57 +1,103 @@
+//---------------------------------------------------------------//
+/**  @CodingBoy100  ~  Pratyush Kargeti  **/
+//---------------------------------------------------------------//
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define ll long long
-#define endl "
-"
+using ll = long long;
 //#define mod  1000000000000000007LL;  
 // const ll mod2= 1000000000000000007LL;  
+const ll INF = (1LL<<60);
 const ll M= 1000000000000000007LL; 
 const ll MOD = 1000000007LL;
-const ll MOD2 = 998244353;
+const ll MOD2 = 998244353LL;
+const int N = 100005;
+#ifdef LOCAL
+#include "debug.hpp"        
+#else
+#define debug(...) 19
+#endif
+// #include "pbds.hpp"
+using ull = unsigned long long;
+#define endl "
+"
+#define pb push_back
+#define tc int t; cin>>t; while(t--)
+ll TESTCASE=0;
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define ri(x) int x; cin >> x
+#define rll(x) ll x; cin >> x
+#define rs(s) string s; cin >> s
+#define rc(c) char c; cin >> c
+#define lb lower_bound
+#define ub upper_bound
+#define sp cout << ' '
+#define ln cout << '
+'
+#define p(x) cout << x
+#define pln(x) cout << x << '
+'
+#define pdln(x) cout<<fixed<<setprecision(10)<<(x)<<'
+'
+#define yn(cond) cout << ((cond) ? "YES
+" : "NO
+")
+template<typename T> using vv = vector<vector<T>>;
+// ll mygcd(ll a,ll b){return b?mygcd(b,a%b):a;}   //__gcd(a,b)
+// ll mylcm(ll a, ll b) {return a/mygcd(a,b)*b;}
+template<typename T> T gcd(T a, T b){ return std::gcd(a, b); }
+template<typename T> T lcm(T a, T b){ return std::lcm(a, b); }
+template<typename T> void read(T &x){ cin>>x; }
+template<typename T> void read(vector<T>& v){ for(auto &x:v) read(x); }
+template<typename T> void read(vector<vector<T>>& v){ for(auto &row:v) read(row); }
+template<typename T1, typename T2> ostream& operator<<(ostream& os, const pair<T1,T2>& p){return os<<'('<<p.first<<", "<<p.second<<')';}
+template<typename Container> void out(const Container& c){for(const auto &x : c) cout<<x<<' ';cout<<'
+';}
+template<typename T>  void out(const vv<T>& v){ for(const auto &row:v)  out(row);}
+template<typename T> void uniq(vector<T>& v){sort(all(v)); v.erase(unique(all(v)),v.end());}
+#define f(i,a,b) for(int i=(a); i<(b); i++)
+#define fr(i,a,b) for(int i=(a); i>=(b); i--)
+#define fx(i,a,b,x) for(int i=(a); i<(b); i+=(x))
+#define frx(i,a,b,x) for(int i=(a); i>=(b); i-=(x))
+#define each(x,v) for(auto &x : v)
+using vll = vector<ll>;
+using vs = vector<string>; 
+using vc = vector<char>; 
+using vi = vector<int>; 
+//-------------------------///*@CodingBoy100*/------------------------/
  
 void solve()
-{
-    int n,k;
-    cin>>n>>k; 
-    string s;
-    cin>>s;
+{      
+    ri(n);
+    ri(k);
+ 
+    rs(s);
+ 
+    //logic-  palins have atmost 1 odd frq character, so if there are x odd freqs chars
+    //  then k must be atleast x-1
+ 
     unordered_map<char,int>m;
-    for(char c:s){
-        m[c]++;
+ 
+    each(x,s){
+        m[x]++;
     }
-    //k chars to be removed must be odd freq chars..
-    //coz even freq chars will always be arrangable in palindrome after deletions
+ 
+    int of=0;
+    each(x,m){
+        if(x.second % 2 !=0) of++;
+    }
+ 
+    yn(k >= (of-1));
     
-    int bad=0; //count of odd frequency chars
-    for(auto i:m){
+}  
  
-        if(i.second&1) bad++;  
-    }
-    int allowed=bad-1;  // minimum odd freq chars that must be deleted
- 
-    //(-1 for cases like "awa"..here w need not be deleted..
-    //odd length palindrome has just 1 odd freq char, which has freq 1, which is allowdable to stay) 
- 
-    if(allowed>k) cout<<"NO"<<endl;
-    else cout<<"YES"<<endl;
- 
-}
  
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
  
+    tc{TESTCASE++; debug(TESTCASE); solve();}
  
-  int t; 
-  cin>>t; 
-  while(t--)
-  { 
-      solve();
-  }
- 
- 
-  
     //solve();
     
     ///*@CodingBoy100*/
