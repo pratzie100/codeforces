41e6f315b5899f2897f480c5197a96395a74288f
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2026-09-03 03:45:41 +0530

    Add Вася и носки [A] from Codeforces

--- 460/A - Вася и носки/solution.cpp
+++ 460/A - Вася и носки/solution.cpp
@@ -57,8 +57,6 @@ template<typename Container> void out(const Container& c){for(const auto &x : c)
 template<typename T>  void out(const vv<T>& v){ for(const auto &row:v)  out(row);}
 template<typename T> void uniq(vector<T>& v){sort(all(v)); v.erase(unique(all(v)),v.end());}
 #define Start auto start=chrono::high_resolution_clock::now();
-#define End auto stop=chrono::high_resolution_clock::now(); auto duration=chrono::duration_cast<chrono::nanoseconds>(stop-start); cerr<<"Time taken (local): "<<(long double)duration.count()/1e6<<" ms
-";
 #define f(i,a,b) for(int i=(a); i<(b); i++)
 #define fr(i,a,b) for(int i=(a); i>=(b); i--)
 #define fx(i,a,b,x) for(int i=(a); i<(b); i+=(x))
@@ -105,15 +103,24 @@ void solve()
  
  
 int main() {
-    ios::sync_with_stdio(false); 
-    cin.tie(0); Start;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    Start;
  
     //tc{TESTCASE++; debug(TESTCASE); solve();}
     solve();
     
+    #ifndef ONLINE_JUDGE
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::nanoseconds>(stop-start);
+        cerr << "Time taken (local): "
+            << (long double)duration.count() / 1e6
+            << " ms
+";
+    #endif
  
-    End; 
     //*@CodingBoy100*/ 
-    return 0;   
+    return 0;
 }
  
